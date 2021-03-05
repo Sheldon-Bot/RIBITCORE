3c5e85f7a9082db3ab1e646aac66c2308626cf81
kadenscott <kscottdev@gmail.com>
2021-03-05 08:34:19 -0500

    Add resource initializaation to RobotManager

--- src/main/java/dev/kscott/sheldonbot/RobotManager.java
+++ src/main/java/dev/kscott/sheldonbot/RobotManager.java
@@ -1,15 +1,32 @@
 package dev.kscott.sheldonbot;
 
 import dev.kscott.sheldonbot.script.ScriptThread;
+import dev.kscott.sheldonbot.utils.ResourceCopy;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import sun.misc.Signal;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.jar.JarFile;
+
 public class RobotManager {
+
+    private static final @NonNull String[] resourceDirectoriesToExtract = new String[]{
+            "scripts"
+    };
+
+    /**
+     * {@link ResourceCopy} instance.
+     */
+    private final @NonNull ResourceCopy resourceCopy;
 
 //    private final @NonNull ScriptThread motorThread;
 
-    public RobotManager() {
-        System.out.println("Starting robot programs...");
+    public RobotManager() throws IOException {
+        this.resourceCopy = new ResourceCopy();
+
+        this.initializeResources();
 
         final @NonNull ScriptThread frontThread = startFront();
         final @NonNull ScriptThread backThread = startBack();
@@ -20,6 +37,20 @@ public class RobotManager {
             backThread.interrupt();
 //            motorThread.interrupt();
         });
+    }
+
+    private void initializeResources() throws IOException {
+        final @NonNull Optional<JarFile> jarOpt = this.resourceCopy.jar(App.class);
+
+        if (jarOpt.isEmpty()) {
+            throw new RuntimeException("Failed to locate jar class.");
+        }
+
+        final @NonNull JarFile jarFile = jarOpt.get();
+
+        for (final @NonNull String path : resourceDirectoriesToExtract) {
+            this.resourceCopy.copyResourceDirectory(jarFile, path, new File("./temp/scripts"));
+        }
     }
 
     public ScriptThread startFront() {

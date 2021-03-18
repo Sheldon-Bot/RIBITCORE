e4bd4d4a191bd0f19670054d817a26f04e4b25d6
kadenscott <kscottdev@gmail.com>
2021-03-18 08:44:49 -0400

    add project

---
+++ src/main/java/dev/kscott/ribitback/RibitbackApplication.java
@@ -0,0 +1,13 @@
+package dev.kscott.ribitback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibitbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibitbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kscott/ribitback/RibitbackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kscott.ribitback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibitbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

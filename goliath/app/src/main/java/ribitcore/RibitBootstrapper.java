052cdeff42545d343d7531c309325169bbfda302
kadenscott <kscottdev@gmail.com>
2021-03-29 08:31:18 -0400

    add RibitBootstrapper

--- goliath/app/src/main/java/ribitcore/RibitBootstrapper.java
+++ goliath/app/src/main/java/ribitcore/RibitBootstrapper.java
@@ -1,8 +1,12 @@
 package ribitcore;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
+import ribitcore.motor.MotorController;
+import uk.pigpioj.PigpioConstants;
 import uk.pigpioj.PigpioInterface;
 import uk.pigpioj.PigpioJ;
+
+import java.io.IOException;
 
 public class RibitBootstrapper {
 
@@ -11,8 +15,27 @@ public class RibitBootstrapper {
      */
     private final @NonNull PigpioInterface pigpio = PigpioJ.getImplementation();
 
-    public void start() {
+    /**
+     * Pigpio version.
+     */
+    private final int pigpioVersion = pigpio.getVersion();
 
+    public void start() {
+        // version check
+        if (pigpioVersion < 0) {
+            System.out.println("Invalid version: "+pigpioVersion+", exiting...");
+            return;
+        } else {
+            System.out.println("version: " + pigpioVersion);
+        }
+
+        new Thread(() -> {
+            final @NonNull MotorController motorController = new MotorController(pigpio);
+
+            while (true) {
+                motorController.forwards();
+            }
+        }).start();
     }
 
 }

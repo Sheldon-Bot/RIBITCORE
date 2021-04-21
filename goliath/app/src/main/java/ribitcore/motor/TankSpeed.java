ae7619c0b559ee06f90f1dfa89f574e44fb018b8
kadenscott <kscottdev@gmail.com>
2021-04-21 08:12:42 -0400

    add TankSpeed enum

---
+++ goliath/app/src/main/java/ribitcore/motor/TankSpeed.java
@@ -0,0 +1,27 @@
+package ribitcore.motor;
+
+/**
+ * Represents a tank speed (movement of both motors in unison).
+ */
+public enum TankSpeed {
+
+    /**
+     * The forward tank speed.
+     */
+    FORWARDS,
+
+    /**
+     * The backwards tank speed.
+     */
+    BACKWARDS,
+
+    /**
+     * The left tank speed.
+     */
+    LEFT,
+
+    /**
+     * The right tank speed.
+     */
+    RIGHT
+}

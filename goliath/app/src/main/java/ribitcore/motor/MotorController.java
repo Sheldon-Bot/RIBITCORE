d801a42c3c40a2e5616b4f34b2562b9f95934577
kadenscott <kscottdev@gmail.com>
2021-04-21 08:12:54 -0400

    add MotorController#setTankSpeed

--- goliath/app/src/main/java/ribitcore/motor/MotorController.java
+++ goliath/app/src/main/java/ribitcore/motor/MotorController.java
@@ -18,14 +18,42 @@ public class MotorController extends ThreadManager {
      */
     private final @NonNull Motor rightMotor;
 
+    /**
+     * Constructs {@link MotorController}.
+     */
     public MotorController() {
         this.leftMotor = new Motor(23);
         this.rightMotor = new Motor(24);
     }
 
+    /**
+     * Runs the thread.
+     */
     @Override
     public void runThread() {
+        while (true) {
 
+        }
+    }
+
+    /**
+     * Sets the robot's tank speed.
+     *
+     * @param speed the speed of the robot.
+     */
+    private void setTankSpeed(final @NonNull TankSpeed speed) {
+        final @NonNull MotorSpeed leftSpeed = switch (speed) {
+            case BACKWARDS, RIGHT -> MotorSpeed.REVERSE;
+            case FORWARDS, LEFT -> MotorSpeed.FORWARDS;
+        };
+
+        final @NonNull MotorSpeed rightSpeed = switch (speed) {
+            case BACKWARDS, LEFT -> MotorSpeed.REVERSE;
+            case FORWARDS, RIGHT -> MotorSpeed.FORWARDS;
+        };
+
+        this.leftMotor.setSpeed(leftSpeed);
+        this.rightMotor.setSpeed(rightSpeed);
     }
 
 }

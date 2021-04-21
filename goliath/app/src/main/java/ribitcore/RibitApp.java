e2ce2baf0170c0e5d5db5ec295f47385c43f1618
kadenscott <kscottdev@gmail.com>
2021-04-21 10:48:08 -0400

    initialize resources in RibitApp

--- goliath/app/src/main/java/ribitcore/RibitApp.java
+++ goliath/app/src/main/java/ribitcore/RibitApp.java
@@ -4,11 +4,25 @@ import org.checkerframework.checker.nullness.qual.NonNull;
 import ribitcore.data.DataStore;
 import ribitcore.display.DisplayController;
 import ribitcore.motor.MotorController;
+import ribitcore.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+import java.util.jar.JarFile;
 
 /**
  * The main class that controls RIBIT.
  */
 public class RibitApp {
+
+    /**
+     * A list of directory names to extract.
+     */
+    private static final @NonNull String[] RESOURCE_DIRS = new String[]{
+            "py",
+            "font"
+    };
 
     /**
      * The {@link DataStore} instance.
@@ -29,6 +43,12 @@ public class RibitApp {
      * Constructs {@link RibitApp}.
      */
     public RibitApp() {
+        try {
+            this.initializeResources();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
         this.dataStore = new DataStore();
         this.motorController = new MotorController(dataStore);
         this.displayController = new DisplayController(dataStore);
@@ -40,6 +60,24 @@ public class RibitApp {
     public void start() {
 //        this.motorController.run();
         this.displayController.run();
+    }
+
+    /**
+     * Initializes resources.
+     * @throws IOException if io failed.
+     */
+    private void initializeResources() throws IOException {
+        final @NonNull Optional<JarFile> jarOpt = ResourceUtils.jar(App.class);
+
+        if (jarOpt.isEmpty()) {
+            throw new RuntimeException("Failed to locate jar class.");
+        }
+
+        final @NonNull JarFile jarFile = jarOpt.get();
+
+        for (final @NonNull String path : RESOURCE_DIRS) {
+            ResourceUtils.copyResourceDirectory(jarFile, path, new File("./sheldon"));
+        }
     }
 
 
